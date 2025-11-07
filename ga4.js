65ba5ad8a3554a736f96a7babecec44bec5bfecc
Kyeen <nhukien0603@gmail.com>
2025-11-07 19:22:04 +0700

    add ga4

---
+++ ga4.js
@@ -0,0 +1,14 @@
+(function () {
+  var s = document.createElement("script");
+  s.async = true;
+  s.src = "https://www.googletagmanager.com/gtag/js?id=G-J3GZD7P6VM";
+  document.head.appendChild(s);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  window.gtag = gtag;
+  gtag("js", new Date());
+  gtag("config", "G-J3GZD7P6VM");
+})();
